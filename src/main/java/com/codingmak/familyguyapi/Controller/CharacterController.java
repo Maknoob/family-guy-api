d6af17ffeaf9641aaf3924ebb3f7df9da472d3a4
Maknoob <imak90@gmail.com>
2024-05-22 13:24:26 +0200

    created Controller for episodes and characters

---
+++ src/main/java/com/codingmak/familyguyapi/Controller/CharacterController.java
@@ -0,0 +1,29 @@
+package com.codingmak.familyguyapi.Controller;
+
+public class CharacterController {
+	private String name;
+	private int age;
+	private String from;
+	private String workPlace;
+	private String family;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public String getWorkPlace() {
+		return workPlace;
+	}
+	public String getFamily() {
+		return family;
+	}
+	
+	
+
+}
