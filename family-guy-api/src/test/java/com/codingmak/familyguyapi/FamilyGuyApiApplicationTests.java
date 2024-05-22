a21f2e186fb9ebe7d0c8f754a157ba682cb5d99c
Maknoob <imak90@gmail.com>
2024-05-22 11:55:58 +0200

    Initial commit

---
+++ family-guy-api/src/test/java/com/codingmak/familyguyapi/FamilyGuyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingmak.familyguyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyGuyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
