9cdb97d190c281d1f2c6534332867fb48a7b73c0
Maknoob <imak90@gmail.com>
2024-05-25 11:45:35 +0200

    added the exceptionhandler

--- src/main/java/com/codingmak/familyguyapi/Controller/EpisodesController.java
+++ src/main/java/com/codingmak/familyguyapi/Controller/EpisodesController.java
@@ -3,11 +3,16 @@ package com.codingmak.familyguyapi.Controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codingmak.familyguyapi.Exceptions.EpisodeNotFound;
 import com.codingmak.familyguyapi.Model.EpisodesModel;
 import com.codingmak.familyguyapi.service.EpisodesService;
 
@@ -15,6 +20,7 @@ import com.codingmak.familyguyapi.service.EpisodesService;
 @RequestMapping("/episodes")
 public class EpisodesController {
 	
+	@Autowired
 	private EpisodesService episodesService;
 	
 	public EpisodesController(EpisodesService episodesService) {
@@ -36,7 +42,10 @@ public class EpisodesController {
 		} 
 		return null;
 	}
-
-
+	
+	@ExceptionHandler({EpisodeNotFound.class})
+	public ResponseEntity<String> handleEpisodeNotFound(EpisodeNotFound exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
 
 }
