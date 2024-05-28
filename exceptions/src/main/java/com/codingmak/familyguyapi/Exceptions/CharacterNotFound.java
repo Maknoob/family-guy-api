f37798d054007ae9b6c25292b3c03d8abf0c9797
Maknoob <imak90@gmail.com>
2024-05-28 18:33:48 +0200

    added exception for Character not found

---
+++ exceptions/src/main/java/com/codingmak/familyguyapi/Exceptions/CharacterNotFound.java
@@ -0,0 +1,10 @@
+package com.codingmak.familyguyapi.Exceptions;
+
+public class CharacterNotFound extends RuntimeException {
+
+	public CharacterNotFound(String message) {
+		super(message);
+	}
+	
+	
+}
