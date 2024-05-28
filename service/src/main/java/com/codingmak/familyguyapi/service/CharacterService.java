2fe6d77975185de1a5d9043b26719fccb9d25362
Maknoob <imak90@gmail.com>
2024-05-28 18:33:23 +0200

    added service for all characters, by ID and by Age

--- service/src/main/java/com/codingmak/familyguyapi/service/CharacterService.java
+++ service/src/main/java/com/codingmak/familyguyapi/service/CharacterService.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.codingmak.familyguyapi.Exceptions.CharacterNotFound;
+import com.codingmak.familyguyapi.Exceptions.EpisodeNotFound;
 import com.codingmak.familyguyapi.Model.CharacterModel;
 
 @Service
@@ -21,24 +24,43 @@ public class CharacterService {
 		CharacterModel peter = new CharacterModel(1, "Peter Griffin", 45, "Mexico", "Brewary", "Griffins");
 		CharacterModel lois = new CharacterModel(2, "Lois Griffin", 43, "USA Denver", "Housewife", "Griffins");
 		CharacterModel chris = new CharacterModel(3, "Chris Griffin", 14, "Quahog", "School", "Griffins");
-		CharacterModel stewie = new CharacterModel(4, "Stewie Griffin", 1, "Quahog", "-", "Griffins");
-		CharacterModel meg = new CharacterModel(5, "Meg Griffin", 16, "Quahog", "School", "Griffins");
+		CharacterModel chris2 = new CharacterModel(4, "Chriss Griffin", 14, "Quahog", "School", "Griffins");
+		CharacterModel stewie = new CharacterModel(5, "Stewie Griffin", 1, "Quahog", "-", "Griffins");
+		CharacterModel meg = new CharacterModel(6, "Meg Griffin", 16, "Quahog", "School", "Griffins");
 		
-		charList.addAll(Arrays.asList(peter,lois,chris,stewie,meg));
+		charList.addAll(Arrays.asList(peter,lois,chris,stewie,meg, chris2));
 	}
 
 
 
-	public Optional<CharacterModel> getCharacter(Integer id) {
-		Optional<CharacterModel> optional = Optional.empty();
-		for (CharacterModel characterModel : charList) {
-			
-			if (id == characterModel.getId()) {
-				optional = Optional.of(characterModel);
-				return optional;
-			}
-		}
-		return optional;
+	public Optional<CharacterModel> getCharacterById(Integer id) {
+        return charList.stream()
+                .filter(character -> id.equals(character.getId()))
+                .findFirst()
+                .or(() -> {
+                    throw new EpisodeNotFound("Character not found with ID: " + id);
+                });
+    
 	}
+
+
+
+	public List<CharacterModel> getAllCharacters() {
+		
+		return charList;
+	}
+
+
+
+	public List<CharacterModel> getCharacterByAge(Integer age) {
+        List<CharacterModel> characters = charList.stream()
+                .filter(character -> age.equals(character.getAge()))
+                .collect(Collectors.toList());
+        if (characters.isEmpty()) {
+        	throw new CharacterNotFound("Character not found with AGE: " + age);
+        }
+        return characters;
+                
+    }
 
 }
